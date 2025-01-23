d2b28d59a1913aa3da58aae5c87dd9c668954990
Justin Ruaya <jrruaya1@up.edu.ph>
2025-01-23 14:33:35 +0800

    fmt: card

--- pokemon-search/src/routes/+layout.svelte
+++ pokemon-search/src/routes/+layout.svelte
@@ -4,7 +4,9 @@
 </script>
 <AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
 	<svelte:fragment slot="lead">CaraCaro </svelte:fragment>
-	<h1>Jus' Pokedex Searcher</h1>
+	<h1 class="h1">
+		<span class="bg-gradient-to-br from-red-500 to-yellow-500 bg-clip-text text-transparent box-decoration-clone">Jus' Pokedex Searcher</span>
+	</h1>
 	<svelte:fragment slot="trail"><LightSwitch /></svelte:fragment>
 </AppBar>
 <slot />
